0bd84ce8b18734b15bcf2bbf833c922f372ee6d7
Andrei Lee <54466666+Croyia@users.noreply.github.com>
2023-09-17 18:16:57 +0800

    Add files via upload

--- scripts/dey-website.js
+++ scripts/dey-website.js
@@ -1,13 +1,14 @@
-function alertEmail() {
+/*function alertEmail() {
     const inputEmail = document.getElementById("inputEmail");
     const emailValue = inputEmail.value;
 
     if (emailValue.trim() !== "") {
-        alert("Hello, " + emailValue + " email me at andreilee0822@gmail.com");
+        alert("Hello, " + emailValue + " email me at andreilee0822@gmail.com or click the email logo!");
     } else {
         alert("Please enter your email first.");
     }
-}
+    alert("Message has been sent!");
+}*/
 
 function toggleBrightness(){
     const bodyElement=document.body;
@@ -24,3 +25,20 @@ function toggleBrightness(){
     }
     
 }
+
+const observer = new IntersectionObserver((entries) => { 
+    entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }else{
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+
+
+const hiddenELements = document.querySelectorAll('.hidden');
+hiddenELements.forEach((el) => observer.observe(el))
+
